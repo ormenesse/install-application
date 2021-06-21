c257fb0a8a57b0e24db6d5ec6b01ddddaa90332d
Vinicius Ormenesse <vormenesse@gyramais.com>
2021-06-21 18:44:22 -0300

    changes on app.py

--- app/app.py
+++ app/app.py
@@ -14,21 +14,11 @@ warnings.filterwarnings("ignore")
 app = Flask(__name__)
 CORS(app) #Prevents CORS errors
 
-"""
-@app.route('/')
-def works():
-    return abort(400, 'Bad Request')
-    
-@app.route('/installapp')
-def works():
-    return abort(400, 'Bad Request')
-"""
-
 @app.route('/')
 def works():
 	return ''
     
-@app.route('/paymentCapacityPriceTable/', methods=['GET'])
+@app.route('/installapp/paymentCapacityPriceTable/', methods=['GET'])
 #/paymentCapacityPriceTable?paymentCapacity=1062.825282&riskGroup=3&Partner=GYRA&Fees=True&Iof=True
 def calculate_paymentCapacity():
     
@@ -36,7 +26,7 @@ def calculate_paymentCapacity():
     
     return paymentCapacityPriceTable(request)
     
-@app.route('/priceTable/', methods=['GET'])
+@app.route('/installapp/priceTable/', methods=['GET'])
 #/priceTable?preApproved=123123&Period=12&interestRate=3&Partner=GYRA&Fees=True&Iof=True
 def calculate_preapproved():
     
